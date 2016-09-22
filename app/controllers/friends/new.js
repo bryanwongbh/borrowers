73602bd21b180ec46ddd4ae672f6efb298176546
Bryan <bwbh87@gmail.com>
2016-09-22 23:16:19 +0800

    Added new route to show individual friends and fixed edit form component

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -4,6 +4,7 @@ export default Ember.Controller.extend({
   actions: {
     save(model) {
       console.log('+--- save action called in friends new controller');
+      this.transitionToRoute('friends.show', model);
     },
     cancel() {
       console.log('+--- cancel action called in friends new controller');
