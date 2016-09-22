77440038a0ba7e3902cd6ec44a8712c9bb40be60
Bryan Wong <bwbh87@gmail.com>
2016-09-22 10:49:46 +0000

    Added controller for new friends

--- app/components/friends/edit-form.js
+++ app/components/friends/edit-form.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save() {
+      console.log('+- save action in edit-form component');
+      this.save(this.get('model'));
+    },
+    cancel() {
+      console.log('+- cancel action in edit-form component');
+      this.cancel();
+    }
+  }
 });
