c5f4e0ad1da739d7cc54293c73250bb8615c6d74
Bryan <bwbh87@gmail.com>
2016-09-24 01:26:34 +0800

    Added controllers edit and index for articles

--- app/routes/articles/show.js
+++ app/routes/articles/show.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	actions: {
+    delete(article) {
+      article.destroyRecord().then(() => {
+        this.transitionToRoute('articles.index');
+      });
+    }
+	}
 });
