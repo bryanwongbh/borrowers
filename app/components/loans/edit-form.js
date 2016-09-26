5ba7078608b4b59d0bd56d637f1a3b632d81975a
Bryan <bwbh87@gmail.com>
2016-09-26 14:12:26 +0800

    Added ember power select and ember-get-config addon to make dropdown work

--- app/components/loans/edit-form.js
+++ app/components/loans/edit-form.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	store: Ember.inject.service(),
+	articles: Ember.computed({
+    get() {
+      //
+      // Since we are using Ember.inject.service, we need to call the
+      // store using the get helper
+      //
+      return this.get('store').findAll('article');
+    }
+  }).readOnly()
 });
