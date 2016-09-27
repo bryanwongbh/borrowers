f277f5a284a285ecf74588dff8c2176afff69aa1
Bryan <bwbh87@gmail.com>
2016-09-27 13:13:19 +0800

    Added route hooks to remove unsaved edits and new

--- app/routes/articles/new.js
+++ app/routes/articles/new.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model() {
 		return this.store.createRecord('article');
+	},
+	resetController(controller, isExiting) {
+		console.log('------in resetController Hook for articles new-----')
+	  if (isExiting) {
+	    var model = controller.get('model');
+	    if (model.get('isNew')) {
+	      model.destroyRecord();
+	    }
+	  }
 	}
 });
--- app/routes/loans/new.js
+++ app/routes/loans/new.js
@@ -5,5 +5,14 @@ export default Ember.Route.extend({
     return this.store.createRecord('loan', {
       friend: this.modelFor('friends/show')
     });
-  }
+  },
+  resetController(controller, isExiting) {
+		console.log('------in resetController Hook for loans new-----')
+	  if (isExiting) {
+	    var model = controller.get('model');
+	    if (model.get('isNew')) {
+	      model.destroyRecord();
+	    }
+	  }
+	}
 });
