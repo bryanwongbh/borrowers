d956587ee73c5270c1e401fedc5642f44416e814
Bryan <bwbh87@gmail.com>
2016-09-29 01:27:58 +0800

    Added adapter and fixed index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Route.extend({
 	ajax: Ember.inject.service(),
 	model()  {
-	  return this.get('ajax').request('/friends').then(function(data){
+	  return this.get('ajax').request(`${config.host}/friends`).then(function(data){
 	  	return {
       	friendsCount: data.data.length
     	};
