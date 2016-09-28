43c445154cd92003f25d706212a8bad50c9bacee
Bryan <bwbh87@gmail.com>
2016-09-28 13:35:07 +0800

    Added sorting capability

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -1,12 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model() {
-		return this.store.findAll('friend');
-		
-    // // We now use store.query and pass include in the options
-    // //
+	queryParams: {
+	  sortBy: {
+	    refreshModel: true
+	  },
+	  sortAscending: {
+	    refreshModel: true
+	  }
+	},
 
-    // return this.store.query('friend', {include: 'loans'});
+	model(params) {
+  //
+  // We use now store.query and pass include in the options
+  //
+
+	  let query = { include: 'loans,loans.article' };
+
+	  // We check if this value is passed
+	  if (params.sortBy) {
+	    query.sort = params.sortBy;
+
+	    // If sortBy is passed we check if sortAscending is false,
+	    // and use the JSONAPI syntax for descending sort
+	    // http://jsonapi.org/format/#fetching-sorting
+	    //
+	    if (!params.sortAscending) {
+	      query.sort = `-${query.sort}`;
+	    }
+	  }
+	  return this.store.query('friend', query);
 	}
 });
